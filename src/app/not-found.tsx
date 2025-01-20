2a230b23258d0664b4faceaecfe5d1d06985d4a1
lvjia <1196444919@qq.com>
2025-01-21 05:31:26 +0800

    4

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -8,7 +8,7 @@ export default function NotFound() {
     <div className="max-w-[1024px] mx-auto px-6 h-screen flex flex-col items-center justify-center">
       <h1 className="text-6xl font-bold mb-4">404</h1>
       <p className="text-xl text-default-500 mb-8">
-        抱歉，您访问的页面不存在
+        抱歉，您访问的页面不存在，大白话就是这个页面没人写过
       </p>
       <Button 
         color="primary"
